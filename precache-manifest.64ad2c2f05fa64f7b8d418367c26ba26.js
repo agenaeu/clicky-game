5c8244c066efb868ac4ca8f47d07cc168ac1d30b
Agenaeu <sdrice18@gmail.com>
2018-12-19 20:54:23 -0700

    Updates

---
+++ precache-manifest.64ad2c2f05fa64f7b8d418367c26ba26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "a481f33564984091bf08",
+    "url": "/clicky-game/static/js/main.a481f335.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "a481f33564984091bf08",
+    "url": "/clicky-game/static/css/main.273bd620.chunk.css"
+  },
+  {
+    "revision": "313da5b278ee7428f07dfe0848fcc22a",
+    "url": "/clicky-game/index.html"
+  }
+];
